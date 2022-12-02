4b2ef0b25505f53dae7ef67977a72936db40ecde
Will Medders <wmedders@gmail.com>
2022-12-02 18:00:54 -0500

    Test: helper method .find_bot_index

--- spec/save_princess_spec.rb
+++ spec/save_princess_spec.rb
@@ -14,6 +14,13 @@ RSpec.describe SavePrincess do
     expect(@game.bot_index).to eq([])
     expect(@game.princess_index).to eq([])
   end
+  describe 'helper methods' do
+    it '.find_bot_index returns an array of 2 integers' do
+      expect(@game.find_bot_index(m, grid)).to eq([1, 1])
+      expect(@game.find_bot_index(m, grid)[0]).to be_a(Integer)
+      expect(@game.find_bot_index(m, grid)[1]).to be_a(Integer)
+      expect(@game.find_bot_index(m, grid).length).to eq(2)
+    end
   describe 'displayPathtoPrincess' do
     it 'returns a string' do
       expect(@game.displayPathtoPrincess(@m, @grid)).to be_a(String)

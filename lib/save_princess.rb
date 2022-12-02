1ed9196664866251aa8a30aa18c299fa72d5e31e
Will Medders <wmedders@gmail.com>
2022-12-02 18:59:59 -0500

    Feat: displayPathtoPrincess prints each move on a new line

--- lib/save_princess.rb
+++ lib/save_princess.rb
@@ -9,7 +9,24 @@ class SavePrincess
   end
 
   def displayPathtoPrincess(m, grid)
-    # binding.pry
+    bot_starting_postion(m, grid)
+    princess_location(m, grid)
+    until @bot_index == @princess_index
+      if @bot_index[0] < @princess_index[0]
+        @bot_index[0] += 1
+        puts "DOWN"
+      else
+        @bot_index[0] -= 1
+        puts "UP"
+      end
+      if @bot_index[1] < @princess_index[1]
+        @bot_index[1] += 1
+        puts "RIGHT"
+      else
+        @bot_index[1] -= 1
+        puts "LEFT"
+      end
+    end
     return ""
   end
 

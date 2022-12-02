561b012c17f29cbc8b2fcefc91fdc91fcf4e3d03
Will Medders <wmedders@gmail.com>
2022-12-02 17:53:48 -0500

    Feature: SavePrincess has attributes upon initialization

--- lib/save_princess.rb
+++ lib/save_princess.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class SavePrincess
+  attr_reader :bot_index, :princess_index
+
+  def initialize
+    @bot_index = []
+    @princess_index = []
+  end
+
   def displayPathtoPrincess(m, grid)
+    # binding.pry
     return ""
   end
 end

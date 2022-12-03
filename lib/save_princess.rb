4cc509b051a4a274e0b11d8d4f3f3cf8c185df81
Will Medders <wmedders@gmail.com>
2022-12-02 19:09:57 -0500

    Refactor: got rid of class to satisfy hackerrank

--- lib/save_princess.rb
+++ lib/save_princess.rb
@@ -1,43 +1,37 @@
 require 'pry'
+# attr_reader :bot_index, :princess_index
 
-class SavePrincess
-  attr_reader :bot_index, :princess_index
+@bot_index = []
+@princess_index = []
 
-  def initialize
-    @bot_index = []
-    @princess_index = []
-  end
-
-  def displayPathtoPrincess(m, grid)
-    bot_starting_postion(m, grid)
-    princess_location(m, grid)
-    until @bot_index == @princess_index
-      if @bot_index[0] < @princess_index[0]
-        @bot_index[0] += 1
-        puts "DOWN"
-      else
-        @bot_index[0] -= 1
-        puts "UP"
-      end
-      if @bot_index[1] < @princess_index[1]
-        @bot_index[1] += 1
-        puts "RIGHT"
-      else
-        @bot_index[1] -= 1
-        puts "LEFT"
-      end
+def displayPathtoPrincess(m, grid)
+  bot_starting_postion(m, grid)
+  princess_location(m, grid)
+  until @bot_index == @princess_index
+    if @bot_index[0] < @princess_index[0]
+      @bot_index[0] += 1
+      puts "DOWN"
+    else
+      @bot_index[0] -= 1
+      puts "UP"
     end
-    return ""
+    if @bot_index[1] < @princess_index[1]
+      @bot_index[1] += 1
+      puts "RIGHT"
+    else
+      @bot_index[1] -= 1
+      puts "LEFT"
+    end
   end
+  return ""
+end
 
-  def bot_starting_postion(m, grid)
-    location = grid.index(grid.find { |row| row.include?("m") })
-    @bot_index = [location, location]
-  end
+def bot_starting_postion(m, grid)
+  location = grid.index(grid.find { |row| row.include?("m") })
+  @bot_index = [location, location]
+end
 
-  def princess_location(m, grid)
-    row = grid.find { |row| row.include?("p") }
-    @princess_index << grid.index(row)
-    @princess_index << row.index("p")
-  end
+def princess_location(m, grid)
+  row = grid.find { |row| row.include?("p") }
+  @princess_index = [grid.index(row), row.index("p")]
 end
--- spec/save_princess_spec.rb
+++ spec/save_princess_spec.rb
@@ -1,40 +1,38 @@
 require 'rspec'
 require './lib/save_princess.rb'
 require 'pry'
-RSpec.describe SavePrincess do
+
+RSpec.describe 'save_princess' do
   before :each do
-    @game = SavePrincess.new
     @m = 3
     @grid = Array.new(@m)
     @grid[0] = "---"
     @grid[1] = "-m-"
     @grid[2] = "p--"
   end
-  it 'exists' do
-    expect(@game.bot_index).to eq([])
-    expect(@game.princess_index).to eq([])
-  end
+  
   describe 'helper methods' do
     it '.bot_starting_postion sets @bot_index to coordinates' do
-      @game.bot_starting_postion(@m, @grid)
-      expect(@game.bot_index[0]).to eq(1)
-      expect(@game.bot_index[1]).to eq(1)
-      expect(@game.bot_index.length).to eq(2)
+      bot_starting_postion(@m, @grid)
+      expect(@bot_index[0]).to eq(1)
+      expect(@bot_index[1]).to eq(1)
+      expect(@bot_index.length).to eq(2)
     end
     it '.princess_location sets @princess_index to coordinates' do
-      @game.princess_location(@m, @grid)
-      expect(@game.princess_index[0]).to eq(2)
-      expect(@game.princess_index[1]).to eq(0)
-      expect(@game.princess_index.length).to eq(2)
+      princess_location(@m, @grid)
+      expect(@princess_index[0]).to eq(2)
+      expect(@princess_index[1]).to eq(0)
+      expect(@princess_index.length).to eq(2)
     end
   end
+
   describe 'displayPathtoPrincess' do
     it 'returns a string' do
-      expect(@game.displayPathtoPrincess(@m, @grid)).to be_a(String)
+      expect(displayPathtoPrincess(@m, @grid)).to be_a(String)
     end
     it 'prints each move on a new line' do
       expect do
-        @game.displayPathtoPrincess(@m, @grid)
+        displayPathtoPrincess(@m, @grid)
       end.to output("DOWN\nLEFT\n").to_stdout
     end
   end

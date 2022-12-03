7d1ce32dd72387b5b7e375029aafd0965ba9cb98
Will Medders <wmedders@gmail.com>
2022-12-03 14:41:23 -0500

    Test: .displayPathtoPrincess prints each direction on a new line

--- spec/save_princess_spec.rb
+++ spec/save_princess_spec.rb
@@ -1,39 +1,39 @@
 require 'rspec'
-require './lib/save_princess.rb'
+require_relative '../lib/save_princess.rb'
 require 'pry'
 
-RSpec.describe 'save_princess' do
-  before :each do
-    @m = 3
-    @grid = Array.new(@m)
-    @grid[0] = "---"
-    @grid[1] = "-m-"
-    @grid[2] = "p--"
-  end
-  
-  describe 'helper methods' do
-    it '.bot_starting_postion sets @bot_index to coordinates' do
-      bot_starting_postion(@m, @grid)
-      expect(@bot_index[0]).to eq(1)
-      expect(@bot_index[1]).to eq(1)
-      expect(@bot_index.length).to eq(2)
+RSpec.describe '.displayPathtoPrincess' do
+  context 'grid is 3x3, bot is located in center, princess is located in corner' do
+    before :each do
+      @m = 3
     end
-    it '.princess_location sets @princess_index to coordinates' do
-      princess_location(@m, @grid)
-      expect(@princess_index[0]).to eq(2)
-      expect(@princess_index[1]).to eq(0)
-      expect(@princess_index.length).to eq(2)
-    end
-  end
 
-  describe 'displayPathtoPrincess' do
-    it 'returns a string' do
-      expect(displayPathtoPrincess(@m, @grid)).to be_a(String)
-    end
-    it 'prints each move on a new line' do
+    it 'prints each direction on a new line. Case: princess in TOP LEFT' do
+      grid = ["p--", "-m-", "---"]
       expect do
-        displayPathtoPrincess(@m, @grid)
+        displayPathtoPrincess(@m, grid)
+      end.to output("UP\nLEFT\n").to_stdout
+    end
+
+    it 'prints each direction on a new line. Case: princess in TOP RIGHT' do
+      grid = ["--p", "-m-", "---"]
+      expect do
+        displayPathtoPrincess(@m, grid)
+      end.to output("UP\nRIGHT\n").to_stdout
+    end
+
+    it 'prints each direction on a new line. Case: princess in BOTTOM LEFT' do
+      grid = ["---", "-m-", "p--"]
+      expect do
+        displayPathtoPrincess(@m, grid)
       end.to output("DOWN\nLEFT\n").to_stdout
+    end
+
+    it 'prints each direction on a new line. Case: princess in BOTTOM RIGHT' do
+      grid = ["---", "-m-", "--p"]
+      expect do
+        displayPathtoPrincess(@m, grid)
+      end.to output("DOWN\nRIGHT\n").to_stdout
     end
   end
 end
